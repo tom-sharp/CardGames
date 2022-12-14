5c60b1e68bcb5a5f0069d66bb87351864197d6a2
tom-sharp <dink521g@gmail.com>
2022-12-14 12:00:50 +0100

    added some test code

--- CardGames/Program.cs
+++ CardGames/Program.cs
@@ -7,9 +7,11 @@ namespace CardGames
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("CardGames");
 			var carddeck = new CardDeck();
 			Card card = null;
+
+			Console.WriteLine("CardGames - A Deck of Cards");
+			Console.WriteLine($"Deck Total cards = {carddeck.CardsTotal} Undrawn = {carddeck.CardsLeft}");
 			while (true) {
 				card = carddeck.NextCard();
 				if (card == null) break;
@@ -24,6 +26,30 @@ namespace CardGames
 				Console.Write($"{card.Symbol}  ");
 			}
 			Console.WriteLine("\nEnd");
+			Console.WriteLine($"Deck Total cards = {carddeck.CardsTotal} Undrawn = {carddeck.CardsLeft}");
+
+			var cardstack = new CardStack(decks: 5);
+			Console.WriteLine("CardGames - A Stack of 5 decks");
+			Console.WriteLine($"Stack Total cards = {cardstack.CardsTotal} Undrawn = {cardstack.CardsLeft}");
+			while (true)
+			{
+				card = cardstack.NextCard();
+				if (card == null) break;
+				Console.Write($"{card.Symbol}  ");
+			}
+			Console.WriteLine("\nShuffle...");
+			cardstack.ShuffleCards();
+			while (true)
+			{
+				card = cardstack.NextCard();
+				if (card == null) break;
+				Console.Write($"{card.Symbol}  ");
+			}
+			Console.WriteLine("\nEnd");
+			Console.WriteLine($"Stack Total cards = {cardstack.CardsTotal} Undrawn = {cardstack.CardsLeft}");
+
+
+
 		}
 	}
 }
